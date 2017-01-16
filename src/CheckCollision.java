ba6591f833c27aeaf7a458248c9c29b1cfe3bd29
MatthewZMD <matthewzmd@gmail.com>
2017-01-16 00:58:09 -0500

    Check threads

--- src/CheckCollision.java
+++ src/CheckCollision.java
@@ -1,31 +1,33 @@
 public class CheckCollision implements Runnable{
 
 	public synchronized void run() {
+		while(true){
 //		System.out.println("Collisioning Size: "+Main.organisms.size());
-		for(int i = 0; i < Main.organisms.size(); i++){
-			System.out.println("Checking Collision");
-			Organism o = Main.organisms.get(i);
-			if(Main.player.getX() == o.getX() && Main.player.getY() == o.getY()){
-				if(o instanceof Walker){
-					if(Main.player.getPlantBuff()>0){
-						System.out.println("You attacked the Walker at "+o.getX()+","+o.getY()+"!");
-						((Walker) o).updateHealth(-1);
-					}else{
-						Main.player.updateHealth(-1);
+			for(int i = 0; i < Main.organisms.size(); i++){
+				System.out.println("Checking Collision");
+				Organism o = Main.organisms.get(i);
+				if(Main.player.getX() == o.getX() && Main.player.getY() == o.getY()){
+					if(o instanceof Walker){
+						if(Main.player.getPlantBuff()>0){
+							System.out.println("You attacked the Walker at "+o.getX()+","+o.getY()+"!");
+							((Walker) o).updateHealth(-1);
+						}else{
+							Main.player.updateHealth(-1);
+						}
+					}else if(o instanceof Plant){
+						Main.player.updateHealth(0.5);
+						Main.player.addPlantBuff(30);
+						Main.organisms.remove(i);
 					}
-				}else if(o instanceof Plant){
-					Main.player.updateHealth(0.5);
-					Main.player.addPlantBuff(30);
-					Main.organisms.remove(i);
 				}
+
 			}
-			
-		}
-		try{
-			//Thread.sleep(10);
-			Thread.yield();
-		}catch(Exception e) {
-			e.printStackTrace();
+			try{
+				//Thread.sleep(10);
+				Thread.yield();
+			}catch(Exception e) {
+				e.printStackTrace();
+			}
 		}
 		
 	}
--- src/CheckDeath.java
+++ src/CheckDeath.java
@@ -1,22 +1,24 @@
 public class CheckDeath implements Runnable{
 
 	public synchronized void run(){
+		while(true){
 //		System.out.println("Deathing Size: "+Main.organisms.size());
-		for(int i = 0; i < Main.organisms.size(); i++){
-			System.out.println("Checking Death");
-			Organism o = Main.organisms.get(i);
-			if(o instanceof Walker){
-				if(((Walker) o).getHealth()<=0){
-					Main.organisms.remove(i);
-					System.out.println("Walker died");
+			for(int i = 0; i < Main.organisms.size(); i++){
+				System.out.println("Checking Death");
+				Organism o = Main.organisms.get(i);
+				if(o instanceof Walker){
+					if(((Walker) o).getHealth()<=0){
+						Main.organisms.remove(i);
+						System.out.println("Walker died");
+					}
 				}
 			}
-		}
-		try{
-			//Thread.sleep(10);
-			Thread.yield();
-		}catch(Exception e) {
-			e.printStackTrace();
+			try{
+				//Thread.sleep(10);
+				Thread.yield();
+			}catch(Exception e) {
+				e.printStackTrace();
+			}
 		}
 	}
 	

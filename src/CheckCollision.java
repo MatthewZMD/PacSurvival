19a2c7f7a8daa06fef2a9188f3a46d6f807e23da
MatthewZMD <matthewzmd@gmail.com>
2017-01-18 00:09:19 -0500

    Programming

--- src/CheckCollision.java
+++ src/CheckCollision.java
@@ -8,12 +8,14 @@ public class CheckCollision implements Runnable{
 				Organism o = MainGame.organisms.get(i);
 
 //                try {
-//                    System.out.println(o.getX()+" "+o.getY()+" "+Math.floor(MainGame.player.getY())+" "+Math.floor(MainGame.player.getX())+" "+(MainGame.player.getY() >= o.getX()-0.5)+" "+(MainGame.player.getY() <= o.getX()+0.5)+" "+(MainGame.player.getX() >= o.getY()-0.5)+" "+(MainGame.player.getX() <= o.getY()+0.5));
+//                System.out.println(o.getX()+" "+o.getY()+" "+Math.floor(MainGame.player.getY())+" "+Math.floor(MainGame.player.getX())+" "+(MainGame.player.getY() >= o.getX()-0.5)+" "+(MainGame.player.getY() <= o.getX()+0.5)+" "+(MainGame.player.getX() >= o.getY()-0.5)+" "+(MainGame.player.getX() <= o.getY()+0.5));
+
                 if (o!=null&&Math.floor(MainGame.player.getY()) >= o.getX()-0.5 && Math.floor(MainGame.player.getY()) <= o.getX()+0.5 && Math.floor(MainGame.player.getX()) >= o.getY()-0.5 && Math.floor(MainGame.player.getX()) <= o.getY()+0.5) {
                     if (o instanceof Walker) {
                         if (MainGame.plantRemainTime > 0) {
-                            System.out.println("You attacked the Walker at " + o.getX() + "," + o.getY() + "!");
+                            System.out.print("You attacked the Walker at " + o.getX() + "," + o.getY() + "! ");
                             ((Walker) o).updateHealth(-100);
+                            System.out.println("Walker remaining health: "+((Walker) o).getHealth());
                         } else {
                             System.out.println("You collided with a Walker at " + o.getX() + "," + o.getY());
                             MainGame.remainTime -= 30;

048c8c206a4693ee4297068e58c7a055c13dbe7b
MatthewZMD <matthewzmd@gmail.com>
2017-01-23 12:38:44 -0500

    Programming

--- src/Creature.java
+++ src/Creature.java
@@ -3,42 +3,63 @@
  * @author Matthew
  */
 
-abstract public class Creature extends Organism{
+abstract public class Creature extends Organism {
 
-  //Declare private class variables
-  private double dirX, dirY;
+    //Declare private class variables
+    private double dirX, dirY;
 
-  /**
-   * default class constructor Creature
-   * @param x
-   * @param y
-   * @param dirX
-   * @param dirY
-   */
-  public Creature(double x, double y, double dirX, double dirY){
-      //assign variable values
-      super(x, y);
-      setDirX(dirX);
-      setDirY(dirY);
-  }
+    /**
+     * Create a creature object
+     *
+     * @param x    position
+     * @param y    position
+     * @param dirX direction
+     * @param dirY direction
+     */
+    public Creature(double x, double y, double dirX, double dirY) {
+        //assign variable values
+        super(x, y);
+        setDirX(dirX);
+        setDirY(dirY);
+    }
 
-  /*************Getters and Setters*********/
+    /*************Getters and Setters*********/
 
-  public double getDirX() {
-    return dirX;
-  }
+    /**
+     * Get the x direction
+     *
+     * @return x direction
+     */
+    public double getDirX() {
+        return dirX;
+    }
 
-  public void setDirX(double dirX) {
-    this.dirX = dirX;
-  }
+    /**
+     * Set the x direction
+     *
+     * @param dirX x direction
+     */
+    public void setDirX(double dirX) {
+        this.dirX = dirX;
+    }
 
-  public double getDirY() {
-    return dirY;
-  }
+    /**
+     * Get the y direction
+     *
+     * @return y direction
+     */
+    public double getDirY() {
+        return dirY;
+    }
 
-  public void setDirY(double dirY) {
-    this.dirY = dirY;
-  }
+    /**
+     * Set the y direction
+     *
+     * @param dirY y direction
+     */
+    public void setDirY(double dirY) {
+        this.dirY = dirY;
+    }
 
 
 }
--- src/Leaderboard.java
+++ src/Leaderboard.java
@@ -4,6 +4,7 @@
  */
 
 //Import tools from java library
+
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.ArrayList;
@@ -12,85 +13,87 @@ import java.util.Scanner;
 
 public class Leaderboard {
 
-	//Declare Private Class Variable
-	private ArrayList<Player> players = new ArrayList<>();
-	
-	/**
-	* default constructor Leaderboard
-	* sets the arraylist and adds the current player into it
-	* @param name the String name of current player
-	 * @param score the int score of the current player
-	*/
-	Leaderboard(String name, int score){
-		
-		try{
-			
-			//start the scanner
-			Scanner fileReader = new Scanner(new java.io.File("Leaderboard.txt"));
-			String playerInfo = fileReader.nextLine();
-			int i = 0;
-			
-			//read player information into array
-			while(playerInfo != null){
-				String[] info = playerInfo.split(",");
-				players.add(new Player(info[0],0,0,0,0));
-				//String name,double x, double y, double dirX, double dirY
-				players.get(i).setScore(Integer.parseInt(info[1]));
-				i++;
-				try{
-					playerInfo = fileReader.nextLine();
-				}catch(NoSuchElementException e){
-					break;
-				}
-				
-			}
-			
-			//close scanner
-			fileReader.close();
-			
-			//Add the new player at the end of arraylist
-			players.add(new Player(name,0,0,0,0));
-			players.get(i).setScore(score);
-			//Sort the players based on what scores they got
+    //Declare Private Class Variable
+    private ArrayList<Player> players = new ArrayList<>();
+
+    /**
+     * default constructor Leaderboard
+     * sets the arraylist and adds the current player into it
+     *
+     * @param name  the String name of current player
+     * @param score the int score of the current player
+     */
+    Leaderboard(String name, int score) {
+
+        try {
+
+            //start the scanner
+            Scanner fileReader = new Scanner(new java.io.File("Leaderboard.txt"));
+            String playerInfo = fileReader.nextLine();
+            int i = 0;
+
+            //read player information into array
+            while (playerInfo != null) {
+                String[] info = playerInfo.split(",");
+                players.add(new Player(info[0], 0, 0, 0, 0));
+                //String name,double x, double y, double dirX, double dirY
+                players.get(i).setScore(Integer.parseInt(info[1]));
+                i++;
+                try {
+                    playerInfo = fileReader.nextLine();
+                } catch (NoSuchElementException e) {
+                    break;
+                }
+
+            }
+
+            //close scanner
+            fileReader.close();
+
+            //Add the new player at the end of arraylist
+            players.add(new Player(name, 0, 0, 0, 0));
+            players.get(i).setScore(score);
+            //Sort the players based on what scores they got
             players = sortAndGet();
 
-			//Reprint the list storage file with one new player in the arraylist
-			PrintWriter reWrite = new PrintWriter(new java.io.File("Leaderboard.txt"));
-			for(int j = 0; j < 10; j++){
-				reWrite.println(players.get(j).getName()+","+players.get(j).getScore());
-			}
-			reWrite.close();
-			
-		//Report any exception to console
-		} catch (IOException e){	
-			System.out.println("Error. Cannot find file.");
-		}
-	}
-	
-	/**
-	* sortAndGet
-	* sort the private class variable ArrayList players
-	* @return ArrayList<Protagonist> players, stores all past players with top 10 highest scores
-	*/
-	public ArrayList<Player> sortAndGet(){
-		
-		//Selection sort
-		int max;
-		for(int i = 0; i < players.size(); i++){
-			max = i;
-			for(int j = i; j < players.size(); j++){
-				if(players.get(j).getScore() > players.get(max).getScore()){
-					max = j;
-				}
-			}
-			//Swap biggest and current locations
-			Player placeholder = players.get(i);
-			players.set(i, players.get(max));
-			players.set(max, placeholder);
-		}
-		
-		//return list of players sorted
-		return players;
-	}
-	
+            //Reprint the list storage file with one new player in the arraylist
+            PrintWriter reWrite = new PrintWriter(new java.io.File("Leaderboard.txt"));
+            for (int j = 0; j < 10; j++) {
+                reWrite.println(players.get(j).getName() + "," + players.get(j).getScore());
+            }
+            reWrite.close();
+
+            //Report any exception to console
+        } catch (IOException e) {
+            System.out.println("Error. Cannot find file.");
+        }
+    }
+
+    /**
+     * sortAndGet
+     * sort the private class variable ArrayList players
+     *
+     * @return ArrayList<Protagonist> players, stores all past players with top 10 highest scores
+     */
+    public ArrayList<Player> sortAndGet() {
+
+        //Selection sort
+        int max;
+        for (int i = 0; i < players.size(); i++) {
+            max = i;
+            for (int j = i; j < players.size(); j++) {
+                if (players.get(j).getScore() > players.get(max).getScore()) {
+                    max = j;
+                }
+            }
+            //Swap biggest and current locations
+            Player placeholder = players.get(i);
+            players.set(i, players.get(max));
+            players.set(max, placeholder);
+        }
+
+        //return list of players sorted
+        return players;
+    }
+
 }
